f1627ba6fcd0e1990dfcbe71b98bb3ea28053848
tacosontitan <jkdavis1691@gmail.com>
2022-02-10 20:00:48 -0600

    Updated main to use the proper path to index.

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ function createWindow () {
   });
 
   // and load the index.html of the app.
-  win.loadFile('index.html');
+  win.loadFile('pages/index.html');
 }
 
 app.on('ready', createWindow);
