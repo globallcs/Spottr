53d6f8a2244435e784d473ecd93ebb1012042832
tacosontitan <jkdavis1691@gmail.com>
2022-02-10 21:38:08 -0600

    Updated to use chakra ui for the landing.

--- src/react.tsx
+++ src/react.tsx
@@ -1,8 +1,17 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
- 
-const Index = () => {
-    return <h1>Spottr</h1>;
-};
- 
-ReactDOM.render(<Index />, document.getElementById('app'));
+import { ChakraProvider, Flex, Heading } from "@chakra-ui/react";
+import * as React from "react";
+import { render } from "react-dom";
+
+function App() {
+  return <Flex w="100%" h="100%" align="center" justify="center" bgGradient='linear(to-r, #6a11cb, #2575fc)' color='white'>
+      <Heading>SPOTTR</Heading>
+    </Flex>;
+}
+
+const rootElement = document.getElementById("app");
+render(
+  <ChakraProvider>
+    <App />
+  </ChakraProvider>,
+  rootElement
+);
