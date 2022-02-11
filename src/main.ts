f0788a73977766f88e027477f72fc0ff93de451f
tacosontitan <jkdavis1691@gmail.com>
2022-02-10 20:03:08 -0600

    Updated gitignore to ignore the dist folder. Updated path to index in main.

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ function createWindow () {
   });
 
   // and load the index.html of the app.
-  win.loadFile('pages/index.html');
+  win.loadFile('index.html');
 }
 
 app.on('ready', createWindow);
