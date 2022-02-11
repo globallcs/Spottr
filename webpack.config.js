c57493f00ea0d0bb7634e3d144d2dd004a78c827
tacosontitan <jkdavis1691@gmail.com>
2022-02-10 19:57:34 -0600

    Added renderer configuration to webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = [
     {
       mode: 'development',
@@ -14,5 +15,25 @@ module.exports = [
         path: __dirname + '/src',
         filename: 'electron.js'
       }
-    }
+    },
+    {
+        mode: 'development',
+        entry: './src/react.tsx',
+        target: 'electron-renderer',
+        devtool: 'source-map',
+        module: { rules: [{
+          test: /\.ts(x?)$/,
+          include: /src/,
+          use: [{ loader: 'ts-loader' }]
+        }] },
+        output: {
+          path: __dirname + '/dist',
+          filename: 'react.js'
+        },
+        plugins: [
+          new HtmlWebpackPlugin({
+            template: './src/index.html'
+          })
+        ]
+      }
   ];
